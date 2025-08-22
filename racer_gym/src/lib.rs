466991069575b6b256eddf5e03b28118b09b29ed
Stepan Mracek <stepan.mracek@threatmark.com>
2025-08-22 23:52:33 +0200

    proper implementation of racer_gym inspired by OpenAI Gymnasium

--- racer_gym/src/lib.rs
+++ racer_gym/src/lib.rs
@@ -1,21 +1,41 @@
 use pyo3::prelude::*;
-use racer_logic::environment::{Action, Environment};
 
-#[pyfunction]
-fn env_step(seed: Option<u64>) -> PyResult<f32> {
-    let mut env = Environment::new(seed);
-    let outcome = env.step(
-        &Action {
-            steer: 0.0,
-            throttle: 1.0,
-        },
-        true,
-    );
-    Ok(outcome.reward)
+#[pyclass(unsendable)]
+struct Environment {
+    env: racer_logic::environment::Environment,
+}
+
+#[pymethods]
+impl Environment {
+    #[new]
+    #[pyo3(signature = (seed=0))]
+    pub fn new(seed: Option<u64>) -> Self {
+        Self {
+            env: racer_logic::environment::Environment::new(seed),
+        }
+    }
+
+    pub fn step(&mut self, steer: f32, throttle: f32) -> (Vec<f32>, f32, bool) {
+        let action = racer_logic::environment::Action { steer, throttle };
+        let outcome = self.env.step(&action, true);
+
+        let observation: Vec<f32> = self.env.observation.clone().into();
+        (observation, outcome.reward, outcome.finished)
+    }
+
+    fn observation(&self) -> Vec<f32> {
+        self.env.observation.clone().into()
+    }
+
+    #[pyo3(signature = (seed=0))]
+    pub fn reset(&mut self, seed: Option<u64>) -> Vec<f32> {
+        self.env = racer_logic::environment::Environment::new(seed);
+        self.observation()
+    }
 }
 
 #[pymodule]
 fn racer_gym(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(env_step, m)?)?;
+    m.add_class::<Environment>()?;
     Ok(())
 }
