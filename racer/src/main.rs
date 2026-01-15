0fbf8c94990f8dc3d3ab46ec1c47b984ec0b53b4
Štěpán Mráček <stepan.mracek@gmail.com>
2026-01-15 22:08:51 +0100

    argparse for controller

--- racer/src/main.rs
+++ racer/src/main.rs
@@ -1,26 +1,42 @@
+use clap::Parser;
 use macroquad::prelude::*;
 use racer_logic::{
     controller::{Controller, KeyboardController},
     environment::Environment,
     states::{Init, State},
 };
+use racer_ndarray_controller::NdarrayController;
 use racer_onnx_controller::{ActionSelectionStrategy, OnnxController};
+
+#[derive(Parser, Debug)]
+struct Args {
+    #[arg(long, value_name = "ONNX_MODEL_PATH")]
+    onnx: Option<String>,
+
+    #[arg(long, value_name = "NDARRAY_MODEL_PATH")]
+    ndarray: Option<String>,
+}
 
 fn window_conf() -> Conf {
     Conf {
         window_title: "racer".to_owned(),
         //fullscreen: true,
         sample_count: 2,
+        window_resizable: true,
         ..Default::default()
     }
 }
 
 fn controller_factory() -> Box<dyn Controller> {
-    if let Some(path) = std::env::args().nth(1) {
+    let args = Args::parse();
+
+    if let Some(path) = args.onnx {
         Box::new(OnnxController::new(
             &path,
             ActionSelectionStrategy::Stochastic,
         ))
+    } else if let Some(path) = args.ndarray {
+        Box::new(NdarrayController::load(&path))
     } else {
         Box::new(KeyboardController::default())
     }
