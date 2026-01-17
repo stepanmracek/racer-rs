559198d0617c8dda3555acc5a0ea49fac279117a
Štěpán Mráček <stepan.mracek@gmail.com>
2026-01-17 17:40:34 +0100

    split ga and train_ndarray_controller

--- train_ndarray_controller/src/main.rs
+++ train_ndarray_controller/src/main.rs
@@ -1,4 +1,3 @@
-use kdam::tqdm;
 use ndarray::Array2;
 use ndarray_rand::RandomExt;
 use ndarray_rand::rand_distr::Normal;
@@ -7,27 +6,8 @@ use racer_logic::{
     environment::{Environment, ReachFinish},
 };
 use racer_ndarray_controller::{Layer, NdarrayController};
-use rand::Rng;
 
-struct GASettings {
-    elite: usize,
-    parents: usize,
-    selected_pairs: usize,
-    childred_per_pair: usize,
-    mutation: f32,
-}
-
-impl Default for GASettings {
-    fn default() -> Self {
-        Self {
-            elite: 50,
-            parents: 200,
-            selected_pairs: 195,
-            childred_per_pair: 10,
-            mutation: 0.1,
-        }
-    }
-}
+//mod ga;
 
 fn evaluate(controller: &mut NdarrayController, env_seed: u64) -> f32 {
     let mut env = Environment::new(Some(env_seed), 0.0, Box::new(ReachFinish::default()));
@@ -44,12 +24,6 @@ fn evaluate(controller: &mut NdarrayController, env_seed: u64) -> f32 {
     reward
 }
 
-fn evaluate_population(population: &mut [NdarrayController], env_seed: u64) -> Vec<f32> {
-    tqdm!(population.iter_mut())
-        .map(|ctrl| evaluate(ctrl, env_seed))
-        .collect()
-}
-
 fn cross(
     first: &NdarrayController,
     second: &NdarrayController,
@@ -58,13 +32,9 @@ fn cross(
 ) -> NdarrayController {
     let layers: Vec<Layer> = std::iter::zip(first.layers.iter(), second.layers.iter())
         .map(|(first, second)| {
-            /*let w_std = (first.w.std(0.0) + second.w.std(0.0)) / 2.0;
-            let b_std = (first.b.std(0.0) + second.b.std(0.0)) / 2.0;*/
             let w = ratio * &first.w + (1.0 - ratio) * &second.w;
             let b = ratio * &first.b + (1.0 - ratio) * &second.b;
             Layer {
-                /*w: &w + noise_std * Array2::random(w.raw_dim(), Normal::new(0.0, w_std).unwrap()),
-                b: &b + noise_std * Array2::random(b.raw_dim(), Normal::new(0.0, b_std).unwrap()),*/
                 w: &w + Array2::random(w.raw_dim(), Normal::new(0.0, noise_std).unwrap()),
                 b: &b + Array2::random(b.raw_dim(), Normal::new(0.0, noise_std).unwrap()),
             }
@@ -74,68 +44,17 @@ fn cross(
     NdarrayController::new(layers)
 }
 
-fn random_pair(len: usize, rng: &mut impl Rng) -> (usize, usize) {
-    let i = rng.random_range(0..len);
-    let offset = rng.random_range(1..len);
-    let j = (i + offset) % len;
-    (i, j)
-}
-
-fn ga_step(
-    mut population: Vec<NdarrayController>,
-    generation: u64,
-    settings: &GASettings,
-) -> Vec<NdarrayController> {
-    let rewards = evaluate_population(&mut population, generation);
-    let avg_reward = rewards.iter().sum::<f32>() / rewards.len() as f32;
-
-    let mut pop_with_rewards: Vec<_> = std::iter::zip(population, rewards).collect();
-    pop_with_rewards.sort_by(|(_, a), (_, b)| b.total_cmp(a));
-
-    println!("{},{},{}", generation, pop_with_rewards[0].1, avg_reward);
-
-    pop_with_rewards[0].0.save(&format!(
-        "research/ndarray_controllers/{generation:05}.json"
-    ));
-
-    let mut new_generation: Vec<_> = pop_with_rewards
-        .iter()
-        .take(settings.elite)
-        .map(|(ctrl, _)| ctrl.clone())
-        .collect();
-
-    let parents_pool: Vec<_> = pop_with_rewards
-        .iter()
-        .take(settings.parents)
-        .map(|(ctrl, _)| ctrl)
-        .collect();
-
-    let mut rng = rand::rng();
-    for _ in 0..settings.selected_pairs {
-        let (first, second) = random_pair(settings.parents, &mut rng);
-
-        for ratio in ndarray::linspace(0.1, 0.9, settings.childred_per_pair) {
-            let mut_coef = 1.0; // rng.random::<f32>();
-            new_generation.push(cross(
-                parents_pool[first],
-                parents_pool[second],
-                ratio,
-                mut_coef * settings.mutation,
-            ));
-        }
-    }
-
-    new_generation
-}
-
 fn main() {
-    let settings = GASettings::default();
-    let init_size = settings.elite + settings.selected_pairs * settings.childred_per_pair;
+    let settings = ga::Settings::default();
+    let init_size = settings.init_size();
+    let ga = ga::GA::new(settings, evaluate, cross);
     let mut population: Vec<_> = (0..init_size)
         .map(|_| NdarrayController::random())
         .collect();
 
     for generation in 0.. {
-        population = ga_step(population, generation, &settings);
+        population = ga.step(
+            population, generation, None, // Some("research/ndarray_controllers/"),
+        );
     }
 }
